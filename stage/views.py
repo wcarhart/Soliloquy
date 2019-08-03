7cf55120a3873cc75129e2b78192bcd9feabdc39
Will Carhart <wcarhart@sandiego.edu>
2019-08-03 10:31:44 -0700

    Prototype App

--- stage/views.py
+++ stage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import App
+from random import shuffle
 
 # Create your views here.
+def stage(request):
+	apps = App.objects.all()
+	shuffle(apps)
+	context = {
+		'apps': apps,
+	}
+	return render(request, 'stage.html', context)
