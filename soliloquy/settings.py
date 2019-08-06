9bf5f82aac420858177de15b757605195cdb9d5f
Will Carhart <wcarhart@sandiego.edu>
2019-08-06 00:21:16 -0700

    Switched to postgres for deploy

--- soliloquy/settings.py
+++ soliloquy/settings.py
@@ -75,17 +75,28 @@ WSGI_APPLICATION = 'soliloquy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': dj_database_url.config(
 
     )
+}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        'NAME': 'soliloquy_db',
+        'USER': 'soliloquy_admin',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 
