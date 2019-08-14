fd8fee9a077dcaef7cab85666fb01f205f471e0b
Will Carhart <wcarhart@sandiego.edu>
2019-08-13 23:40:15 -0700

    Added architect statement

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -8,3 +8,4 @@ set -e
 git push soliloquy-dev "$TRAVIS_PULL_REQUEST_BRANCH":master
 heroku run --app soliloquy-dev python manage.py makemigrations
 heroku run --app soliloquy-dev python manage.py migrate
+python architect.py
