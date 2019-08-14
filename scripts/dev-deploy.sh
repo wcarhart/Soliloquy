a59bcffc43db6e54c71a6e7d93fd9dd28bdd747f
Will Carhart <wcarhart@sandiego.edu>
2019-08-13 23:56:09 -0700

    Updated git

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-git checkout -b "$TRAVIS_PULL_REQUEST_BRANCH" origin/"$TRAVIS_PULL_REQUEST_BRANCH"
+git fetch
+git checkout "$TRAVIS_PULL_REQUEST_BRANCH"
 git push soliloquy-dev "$TRAVIS_PULL_REQUEST_BRANCH":master
 heroku run --app soliloquy-dev python manage.py makemigrations
 heroku run --app soliloquy-dev python manage.py migrate
