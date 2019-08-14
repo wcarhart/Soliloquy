9b47b87494073dc112a4a1692892e2c32b08f4ce
Will Carhart <wcarhart@sandiego.edu>
2019-08-13 23:50:07 -0700

    Updated Heroku

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-heroku login
 git push soliloquy-dev "$TRAVIS_PULL_REQUEST_BRANCH":master
 heroku run --app soliloquy-dev python manage.py makemigrations
 heroku run --app soliloquy-dev python manage.py migrate
