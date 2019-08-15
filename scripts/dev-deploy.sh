5222e9bf5fa65dfabdb7eb3ac05d47421cca77b8
Will Carhart <wcarhart@sandiego.edu>
2019-08-14 21:17:10 -0700

    Updated heroku credentials

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
+heroku login
 git fetch
+echo "git checkout -b $TRAVIS_PULL_REQUEST_BRANCH origin/$TRAVIS_PULL_REQUEST_BRANCH"
 git checkout -b "$TRAVIS_PULL_REQUEST_BRANCH" origin/"$TRAVIS_PULL_REQUEST_BRANCH"
 git push https://git.heroku.com/soliloquy-dev.git "$TRAVIS_PULL_REQUEST_BRANCH":master
 heroku run --app soliloquy-dev python manage.py makemigrations
