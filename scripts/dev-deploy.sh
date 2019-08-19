312037076e5364fa6024ddba0acbe53e0d96b59f
Will Carhart <wcarhart@sandiego.edu>
2019-08-18 17:21:28 -0700

    Hiding logs

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -4,11 +4,11 @@ set -x
 git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 yes | heroku keys:clear > /dev/null 2>&1
 yes | heroku keys:add > /dev/null 2>&1
-git fetch
-echo "git checkout -b $TRAVIS_PULL_REQUEST_BRANCH origin/$TRAVIS_PULL_REQUEST_BRANCH"
-git checkout -b "$TRAVIS_PULL_REQUEST_BRANCH" origin/"$TRAVIS_PULL_REQUEST_BRANCH"
-git remote add heroku git@heroku.com:soliloquy-dev.git
-yes | git push heroku "$TRAVIS_PULL_REQUEST_BRANCH":master
-heroku run --app soliloquy-dev python manage.py makemigrations
-heroku run --app soliloquy-dev python manage.py migrate
-heroku run --app soliloquy-dev python architect.py
+git fetch > /dev/null 2>&1
+echo "git checkout -b $TRAVIS_PULL_REQUEST_BRANCH origin/$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null 2>&1
+git checkout -b "$TRAVIS_PULL_REQUEST_BRANCH" origin/"$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null 2>&1
+git remote add heroku git@heroku.com:soliloquy-dev.git > /dev/null 2>&1
+yes | git push heroku "$TRAVIS_PULL_REQUEST_BRANCH":master > /dev/null 2>&1
+heroku run --app soliloquy-dev python manage.py makemigrations > /dev/null 2>&1
+heroku run --app soliloquy-dev python manage.py migrate > /dev/null 2>&1
+heroku run --app soliloquy-dev python architect.py > /dev/null 2>&1
