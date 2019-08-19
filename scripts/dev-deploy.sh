08d4b1d43dc8eb398a1b793d1bb643b4c7fea01e
Will Carhart <wcarhart@sandiego.edu>
2019-08-18 17:26:09 -0700

    Typo fix

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -5,10 +5,10 @@ git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o Stri
 yes | heroku keys:clear > /dev/null 2>&1
 yes | heroku keys:add > /dev/null 2>&1
 git fetch > /dev/null
-echo "git checkout -b $TRAVIS_PULL_REQUEST_BRANCH origin/$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null
-git checkout -b "$TRAVIS_PULL_REQUEST_BRANCH" origin/"$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null
-git remote add heroku git@heroku.com:soliloquy-dev.git > /dev/null
-yes | git push heroku "$TRAVIS_PULL_REQUEST_BRANCH":master > /dev/null
-heroku run --app soliloquy-dev python manage.py makemigrations > /dev/null
-heroku run --app soliloquy-dev python manage.py migrate > /dev/null
-heroku run --app soliloquy-dev python architect.py > /dev/null
+echo "git checkout -b $TRAVIS_PULL_REQUEST_BRANCH origin/$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null 2>&1
+git checkout -b "$TRAVIS_PULL_REQUEST_BRANCH" origin/"$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null 2>&1
+git remote add heroku git@heroku.com:soliloquy-dev.git > /dev/null 2>&1
+yes | git push heroku "$TRAVIS_PULL_REQUEST_BRANCH":master > /dev/null 2>&1
+heroku run --app soliloquy-dev python manage.py makemigrations > /dev/null 2>&1
+heroku run --app soliloquy-dev python manage.py migrate > /dev/null 2>&1
+heroku run --app soliloquy-dev python architect.py > /dev/null 2>&1
