44c516e950a13e2b2d1873a5a14ed3d54ceeea60
Will Carhart <wcarhart@sandiego.edu>
2019-08-18 18:01:15 -0700

    Debugging dev deploy

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -9,6 +9,6 @@ echo "git checkout -b $TRAVIS_PULL_REQUEST_BRANCH origin/$TRAVIS_PULL_REQUEST_BR
 git checkout -b "$TRAVIS_PULL_REQUEST_BRANCH" origin/"$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null 2>&1
 git remote add heroku git@heroku.com:soliloquy-dev.git > /dev/null 2>&1
 yes | git push heroku "$TRAVIS_PULL_REQUEST_BRANCH":master > /dev/null 2>&1
-heroku run --app soliloquy-dev python manage.py makemigrations > /dev/null 2>&1
-heroku run --app soliloquy-dev python manage.py migrate > /dev/null 2>&1
-heroku run --app soliloquy-dev python architect.py > /dev/null 2>&1
+heroku run --app soliloquy-dev python manage.py makemigrations
+heroku run --app soliloquy-dev python manage.py migrate
+heroku run --app soliloquy-dev python architect.py
