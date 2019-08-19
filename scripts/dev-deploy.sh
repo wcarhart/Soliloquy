f36eedc76004c27629cd0eaeca1d6869fda72f0b
Will Carhart <wcarhart@sandiego.edu>
2019-08-18 17:06:25 -0700

    Update git ssh option

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 set -x
+git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 yes | heroku keys:clear > /dev/null 2>&1
 yes | heroku keys:add > /dev/null 2>&1
 git fetch
