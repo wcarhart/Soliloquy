08691183db270b0ad9e19477c447a84d582121aa
Will Carhart <wcarhart@sandiego.edu>
2019-08-18 17:02:05 -0700

    Automate git push

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -7,7 +7,7 @@ git fetch
 echo "git checkout -b $TRAVIS_PULL_REQUEST_BRANCH origin/$TRAVIS_PULL_REQUEST_BRANCH"
 git checkout -b "$TRAVIS_PULL_REQUEST_BRANCH" origin/"$TRAVIS_PULL_REQUEST_BRANCH"
 git remote add heroku git@heroku.com:soliloquy-dev.git
-git push heroku "$TRAVIS_PULL_REQUEST_BRANCH":master
+yes | git push heroku "$TRAVIS_PULL_REQUEST_BRANCH":master
 heroku run --app soliloquy-dev python manage.py makemigrations
 heroku run --app soliloquy-dev python manage.py migrate
 heroku run --app soliloquy-dev python architect.py
