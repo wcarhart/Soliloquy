7008d07e296cde19ee8933c86ddd27c2965519ac
Will Carhart <wcarhart@sandiego.edu>
2019-08-18 18:11:59 -0700

    Fixing prod deploy

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -5,7 +5,6 @@ git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o Stri
 yes | heroku keys:clear > /dev/null 2>&1
 yes | heroku keys:add > /dev/null 2>&1
 git fetch > /dev/null
-echo "git checkout -b $TRAVIS_PULL_REQUEST_BRANCH origin/$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null 2>&1
 git checkout -b "$TRAVIS_PULL_REQUEST_BRANCH" origin/"$TRAVIS_PULL_REQUEST_BRANCH" > /dev/null 2>&1
 git remote add heroku git@heroku.com:soliloquy-dev.git > /dev/null 2>&1
 yes | git push heroku "$TRAVIS_PULL_REQUEST_BRANCH":master > /dev/null 2>&1
